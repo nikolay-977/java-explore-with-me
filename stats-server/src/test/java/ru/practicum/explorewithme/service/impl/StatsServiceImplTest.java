822ab4ab1aeddf09506fbd732f71cb891c42f06d
Николай Костин <nikolay-977@yandex-team.ru>
2022-11-30 16:42:45 +0300

    refactor

--- stats-server/src/test/java/ru/practicum/explorewithme/service/impl/StatsServiceImplTest.java
+++ stats-server/src/test/java/ru/practicum/explorewithme/service/impl/StatsServiceImplTest.java
@@ -5,19 +5,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.practicum.explorewithme.mapper.EndpointHitMapper;
-import ru.practicum.explorewithme.mapper.ViewStatsMapper;
-import ru.practicum.explorewithme.model.EndpointHit;
-import ru.practicum.explorewithme.model.ViewStats;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Transactional
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
